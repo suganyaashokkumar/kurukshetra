01348b62267fd78cfeda86d2f984a7f13d59d7fe
prakashdhanas <prakash.finding@gmail.com>
2014-05-17 10:39:51 +0530

    first push

---
+++ button.js
@@ -0,0 +1,37 @@
+
+ function home()
+ {
+   window.location.assign("home.html")
+  }
+ function events()
+ {
+   window.location.assign("events.html")
+  }
+  function workshops()
+ {
+   window.location.assign("workshops.html")
+  }
+  function lectures()
+ {
+   window.location.assign("lectures.html")
+  }
+  function highlights()
+ {
+   window.location.assign("highlights.html")
+  }
+  function conclave()
+ {
+   window.location.assign("conclave.html")
+  }
+  function sponsors()
+ {
+   window.location.assign("sponsors.html")
+  }
+  function forum()
+ {
+   window.location.assign("forum.html")
+  }
+  function hospi()
+ {
+   window.location.assign("hospi.html")
+  }
